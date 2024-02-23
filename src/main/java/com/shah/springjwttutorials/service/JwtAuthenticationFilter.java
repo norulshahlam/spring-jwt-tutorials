b578da7a0872e1895cccecacc3d7f5c273f9a876
norulshahlam <45616013+norulshahlam@users.noreply.github.com>
2024-02-23 19:43:15 +0800

    Update JwtAuthenticationFilter.java

--- src/main/java/com/shah/springjwttutorials/service/JwtAuthenticationFilter.java
+++ src/main/java/com/shah/springjwttutorials/service/JwtAuthenticationFilter.java
@@ -6,7 +6,6 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
-import org.apache.commons.lang3.ObjectUtils;
 import org.springframework.lang.NonNull;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -17,6 +16,9 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+
+import static org.apache.commons.lang3.ObjectUtils.isEmpty;
+import static org.apache.commons.lang3.ObjectUtils.isNotEmpty;
 
 
 /**
@@ -39,7 +41,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
          * SecurityConfig.class
          */
         final String authHeader = request.getHeader("Authorization");
-        if (ObjectUtils.isEmpty(authHeader) || !authHeader.startsWith("Bearer ")) {
+        if (isEmpty(authHeader) || !authHeader.startsWith("Bearer ")) {
             filterChain.doFilter(request, response);
             return;
         }
@@ -50,7 +52,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
          */
         final String jwt = authHeader.substring(7);
         final String userEmail = jwtService.extractUsername(jwt);
-        if (userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+        if (isNotEmpty(userEmail) && isEmpty(SecurityContextHolder.getContext().getAuthentication())) {
             UserDetails userDetails = this.userDetailsService.loadUserByUsername(userEmail);
 
             if (jwtService.isTokenValid(jwt, userDetails)) {
