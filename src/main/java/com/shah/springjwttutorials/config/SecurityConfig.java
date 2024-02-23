08a1f5936d45a98c9def123f9f55cd6bf85260f9
norulshahlam <45616013+norulshahlam@users.noreply.github.com>
2024-02-23 16:42:19 +0800

    Update SecurityConfig.java

--- src/main/java/com/shah/springjwttutorials/config/SecurityConfig.java
+++ src/main/java/com/shah/springjwttutorials/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import com.shah.springjwttutorials.service.UserDetailsServiceImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
@@ -128,7 +129,7 @@ public class SecurityConfig {
         return config.getAuthenticationManager();
     }
     @Bean
-    public DaoAuthenticationProvider authenticationProvider() {
+    public AuthenticationProvider authenticationProvider() {
         DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
         authProvider.setUserDetailsService(userDetailsService);
         authProvider.setPasswordEncoder(passwordEncoder());
