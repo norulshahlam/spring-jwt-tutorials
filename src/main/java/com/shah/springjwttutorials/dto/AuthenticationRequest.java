d373a49a1630fdc177a9e1fc2d8138d2559c52bf
norulshahlam <45616013+norulshahlam@users.noreply.github.com>
2024-02-23 19:51:18 +0800

    Update AuthenticationRequest.java

--- src/main/java/com/shah/springjwttutorials/dto/AuthenticationRequest.java
+++ src/main/java/com/shah/springjwttutorials/dto/AuthenticationRequest.java
@@ -1,19 +1,28 @@
 package com.shah.springjwttutorials.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+/**
+ * @author NORUL
+ */
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class AuthenticationRequest {
 
+  @NotEmpty
+  @Email
   @Schema(example = "admin@gmail.com")
   private String email;
+  @NotBlank
   @Schema(example = "1234")
   private String password;
 }
