cc3b05999b5d8bdf2edb168e8902bc668fcefc5d
norulshahlam <45616013+norulshahlam@users.noreply.github.com>
2024-02-24 16:28:07 +0800

    Create MyResponse.java

---
+++ src/main/java/com/shah/springjwttutorials/dto/MyResponse.java
@@ -0,0 +1,40 @@
+package com.shah.springjwttutorials.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.*;
+
+/**
+ * @author NORUL
+ */
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class MyResponse<T> {
+
+    @NonNull
+    ResponseStatus status;
+    T data;
+    String errorMessage;
+
+    public static <T> MyResponse successResponse(T data) {
+        return MyResponse.builder()
+                .status(ResponseStatus.SUCCESS)
+                .data(data)
+                .build();
+    }
+    public static MyResponse failureResponse(String errorMessage) {
+        return MyResponse.builder()
+                .status(ResponseStatus.FAILURE)
+                .errorMessage(errorMessage)
+                .build();
+    }
+    public static <T> MyResponse failureResponse(T data, String errorMessage) {
+        return MyResponse.builder()
+                .status(ResponseStatus.FAILURE)
+                .data(data)
+                .errorMessage(errorMessage)
+                .build();
+    }
+}
