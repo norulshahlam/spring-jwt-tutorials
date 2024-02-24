7a8bea59199fc244fd879a263837631cd94c8540
norulshahlam <45616013+norulshahlam@users.noreply.github.com>
2024-02-24 21:09:06 +0800

    Update GlobalExceptionHandler.java

--- src/main/java/com/shah/springjwttutorials/exception/GlobalExceptionHandler.java
+++ src/main/java/com/shah/springjwttutorials/exception/GlobalExceptionHandler.java
@@ -3,29 +3,76 @@ package com.shah.springjwttutorials.exception;
 import com.shah.springjwttutorials.dto.MyResponse;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
 
 @RestControllerAdvice
 @Slf4j
 public class GlobalExceptionHandler {
 
-    /**
-     * For all other unexpected exceptions
-     *
-     * @param req
-     * @param e
-     * @return
-     */
 
-    @ExceptionHandler(Exception.class)
+    /**
+     * Handles MethodArgumentNotValidException thrown by Spring.
+     *
+     * @param req the HttpServletRequest object
+     * @param e   the MethodArgumentNotValidException object
+     * @return a MyResponse object with an error message
+     */
+    @ExceptionHandler({MethodArgumentNotValidException.class})
     @ResponseBody
-    public ResponseEntity handleBaseException(HttpServletRequest req, Exception e) {
+    public MyResponse<List<Errors>> handleMethodArgumentNotValidException(HttpServletRequest req, MethodArgumentNotValidException e) {
+        String requestUri = req.getRequestURI();
+
+        List<Errors> cause = e.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(error -> Errors.builder()
+                        .fieldName(error.getField())
+                        .errorMessage(error.getDefaultMessage())
+                        .build())
+                .toList();
+
+        log.error("requestUrl : {}, occurred an error : {}", req.getRequestURI(), cause);
+        return MyResponse.failureResponse(cause, "Validation failed for request " +
+                "URI: " + requestUri);
+    }
+
+    /**
+     * Handles BadCredentialsException thrown by Spring Security.
+     *
+     * @param req the HttpServletRequest object
+     * @param e   the BadCredentialsException object
+     * @return a MyResponse object with an error message
+     */
+    @ExceptionHandler(BadCredentialsException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ResponseBody
+    public MyResponse<String> handleBadCredentialsException(HttpServletRequest req, BadCredentialsException e) {
         String errorMessages = e.getLocalizedMessage();
         log.error("requestUrl : {}, occurred an error : {}", req.getRequestURI(), errorMessages);
-        MyResponse failureResponse = MyResponse.failureResponse(errorMessages);
-        return ResponseEntity.badRequest().body(failureResponse);
+        return MyResponse.failureResponse(errorMessages);
+    }
+
+    /**
+     * Handles all unexpected exceptions thrown by the application.
+     *
+     * @param req the HttpServletRequest object
+     * @param e   the Exception object
+     * @return a MyResponse object with an error message
+     */
+    @ExceptionHandler(Exception.class)
+    @ResponseBody
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public MyResponse<String> handleBaseException(HttpServletRequest req, Exception e) {
+        String errorMessages = e.getLocalizedMessage();
+        log.error("requestUrl : {}, occurred an error : {}", req.getRequestURI(), errorMessages);
+        return MyResponse.failureResponse(errorMessages);
     }
 }
